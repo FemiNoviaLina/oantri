ab4a34d21569fdb4ddf7fa5be2c1d49b3087ff45
Femi Novia Lina <feminovi@gmail.com>
2021-12-15 22:18:41 +0700

    add outlet pages

---
+++ js/data.js
@@ -0,0 +1,62 @@
+export const outlets = [
+    {
+        name: "Royal Salon",
+        address: "Tugu, Malang",
+        location: "Hotel Tugu Malang Jalan Tugu No. 3, Jl. Kahuripan No.3, Klojen, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Rawat kecantikan Anda di Royal Salon & Spa, Bekasi. Baik Anda yang menginginkan potongan rambut sederhana atau perawatan spa yang memanjakan, Royal Salon & Spa hadir untuk merawat Anda dari ujung rambut hingga ujung kaki. Manjakan diri dalam paket Spa Bunga di mana Anda akan merasakan pijat relaksasi, batu panas, akupresur aura, dan berbagai perawatan lainnya. Lakukan perawatan wajah untuk membersihkan wajah Anda dari komedo dan kotoran yang menumpuk di wajah Anda atau pilih perawatan dermabrasi emas dan berlian wajah untuk wajah yang sehat dan bebas jerawat.",
+        logo: "../img/royal-salon-logo.jpg",
+        pic: "../img/royal-salon-photo.jpg",
+    }, 
+    {
+        name: "Restoran Melati",
+        address: "Tugu, Malang",
+        location: "Jl. Raya Melati No.1, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Restoran keluarga",
+        logo: "../img/restoran-melati-logo.png",
+        pic: "../img/restoran-melati-photo.jpg"
+    }, 
+    {
+        name: "Gloskin Clinic",
+        address: "Tugu, Malang",
+        location: "Jl. Raya Melati No.12, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Klinik kecantikan",
+        logo: "../img/gloskin-clinic-logo.png",
+        pic: "../img/gloskin-clinic-photo.jpeg"
+    },
+    {
+        name: "House of Pet",
+        address: "Tugu, Malang",
+        location: "Jl. Raya Melati No.13, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Tempat peliharaan",
+        logo: "../img/house-of-pet-logo.png",
+        pic: "../img/house-of-pet-photo.jpg"
+    }, 
+    {
+        name: "Djati Lounge",
+        address: "Tugu, Malang",
+        location: "Jl. Raya Melati No.14, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Tempat makan",
+        logo: "../img/djati-lounge-logo.png"
+    }, 
+    {
+        name: "Smiley Garage",
+        address: "Tugu, Malang",
+        location: "Jl. Raya Melati No.15, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Tempat perawatan kendaraan",
+        logo: "../img/smiley-garage-logo.png"
+    },
+    {
+        name: "Tiara Salon",
+        address: "Tugu, Malang",
+        location: "Jl. Raya Melati No.16, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Tempat perawatan kulit",
+        logo: "../img/tiara-salon-logo.png"
+    }, 
+    {
+        name: "RSUB",
+        address: "Tugu, Malang",
+        location: "Universitas Brawijaya, Jl. Raya Melati No.17, Kec. Klojen, Kota Malang, Jawa Timur 65119",
+        description: "Rumah Sakit Universitas Brawijaya",
+        logo: "../img/rsub-logo.png"
+    }
+]

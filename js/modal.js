fe41b549c86bf1c57613a920dedb86ff522b11ed
Femi Novia Lina <feminovi@gmail.com>
2021-12-19 09:51:31 +0700

    add modal to view image

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("logo-modal");
 
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("outlet-logo");
 
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[1];
 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -15,5 +15,25 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+var modal2 = document.getElementById("photo-modal");
+
+var btn2 = document.getElementById("outlet-photo");
+
+var span2 = document.getElementsByClassName("close")[0];
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
